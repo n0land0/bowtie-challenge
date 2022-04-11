afe9c1e7112d0e805e37e5786acd332f6c347a7b
Nolan Caine <nolancaine@gmail.com>
2022-04-11 00:02:48 -0600

    Confirm useProjects hook and mockData working

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
+
 import logo from './assets/logo.svg';
+import { useProjects } from './lib/useProjects';
 
 function App() {
+  const { loading, error, data } = useProjects();
+  console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
