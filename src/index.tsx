e8374609788ebd26328c90c8c0a10916afb7110f
Nolan Caine <nolancaine@gmail.com>
2022-04-10 23:46:40 -0600

    Add suggested changes for React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import './styles/index.scss';
 
-import App from './App';
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
-render(
+import App from './App';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
