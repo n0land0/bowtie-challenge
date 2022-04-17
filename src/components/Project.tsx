d7d947db5bd7a0ab09e8684f93a703551f5b0aea
Nolan Caine <nolancaine@gmail.com>
2022-04-17 17:52:31 -0600

    Add conditionally rendered form and event handlers

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,11 +1,24 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, {
+  ChangeEvent,
+  FC,
+  FocusEvent,
+  FormEvent,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 
-import { getAllTodosByProject } from '../lib/api';
+import { getAllTodosByProject, getProjects, updateProject } from '../lib/api';
+import { AppContext } from '../lib/context';
 import { Project as ProjectProps, Todo as ITodo } from '../lib/models';
 import Todo from './Todo';
 
 const Project: FC<ProjectProps> = ({ id, projectName }) => {
+  const { setProjects } = useContext(AppContext);
+
   const [todos, setTodos] = useState<ITodo[]>([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [inputValue, setInputValue] = useState(projectName);
 
   useEffect(() => {
     const loadTodos = async () => {
@@ -18,12 +31,49 @@ const Project: FC<ProjectProps> = ({ id, projectName }) => {
     loadTodos();
   }, [id]);
 
+  const toggleIsEditing = () => setIsEditing(!isEditing);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) =>
+    setInputValue(event.target.value);
+
+  const handleBlur = (event: FocusEvent) => toggleIsEditing();
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(inputValue);
+    if (id) {
+      updateProject(inputValue, id).then(() =>
+        getProjects().then((projectData) => {
+          console.log('data after submitting name change', projectData);
+          setProjects(projectData);
+        })
+      );
+    }
+    toggleIsEditing();
+  };
+
   const todoElements = todos.map((todo: ITodo) => (
     <Todo key={todo.id} {...todo} />
   ));
   return (
     <article>
-      <h3>{projectName}</h3>
+      {isEditing ? (
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={inputValue}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <button>save changes</button>
+        </form>
+      ) : (
+        <div>
+          <h3>{projectName}</h3>
+          <button onClick={toggleIsEditing}>edit</button>
+        </div>
+      )}
+
       {todoElements}
     </article>
   );
