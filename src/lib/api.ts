bab844a45347c02094996ee9c9436a16da0ba4ac
Nolan Caine <nolancaine@gmail.com>
2022-04-17 15:38:37 -0600

    Clean up api functions

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,26 @@
 import { Project } from './models';
 
-export const createProject = (params: Project) => {
-  // fetch(apiUrl, {
-  // method: "POST",
-  // body: JSON.stringify(params)
-  // })
-  localStorage.setItem(Date.now().toString(), JSON.stringify(params));
+const apiUrl = '/api/v1';
+
+export const getProjects = () => {
+  return fetch(`${apiUrl}/projects`).then((response) =>
+    checkResponse(response)
+  );
+};
+
+export const createProject = (projectName: string) => {
+  return fetch(`${apiUrl}/projects`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      projectName,
+    }),
+  }).then((response) => checkResponse(response));
+};
+
+const checkResponse = (response: Response) => {
+  if (!response.ok) throw new Error(`${response.status}: bad response`);
+  return response.json();
 };
