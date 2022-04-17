50d20985ec860dde2990b22376c92ed529b38b3f
Nolan Caine <nolancaine@gmail.com>
2022-04-17 15:26:33 -0600

    Add projects to context state

--- src/lib/context.tsx
+++ src/lib/context.tsx
@@ -1,10 +1,24 @@
-import React, { createContext, PropsWithChildren, useState } from 'react';
+import React, {
+  createContext,
+  PropsWithChildren,
+  useEffect,
+  useState,
+} from 'react';
 
 import { AppContextProps, Project, Todo } from './models';
+import { useProjects } from './useProjects';
 
 const AppContext = createContext<AppContextProps>({});
 
 const ContextProvider = ({ children }: PropsWithChildren<AppContextProps>) => {
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  const { loading, error, data } = useProjects();
+
+  useEffect(() => {
+    if (data.length) setProjects(data);
+  }, [data]);
+
   const value = {};
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
