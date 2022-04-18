cd2b77a58e26c2b37b8ad9c0005d50b870c29acd
Nolan Caine <nolancaine@gmail.com>
2022-04-18 02:21:08 -0600

    Style main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Container, Flex, Heading } from '@chakra-ui/react';
 import React, { FC, useEffect } from 'react';
 
 import ProjectsContainer from './components/ProjectsContainer';
@@ -5,11 +6,23 @@ import { createProject, getProjects } from './lib/api';
 
 const App: FC = () => {
   return (
-    <main>
-      <header></header>
+    <Flex
+      as='main'
+      direction='column'
+      w='100vw'
+      h='100vh'
+      m={0}
+      p={0}
+      alignItems='center'
+      bg='gray.300'
+    >
+      <header>
+        <Heading as='h1' size='3xl' m='5'>
+          Todo!
+        </Heading>
+      </header>
       <ProjectsContainer />
-      <footer></footer>
-    </main>
+    </Flex>
   );
 };
 
